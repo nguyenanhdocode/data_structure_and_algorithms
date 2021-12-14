a7cbd703064b4f6f39560ccf16319a788fa99058
Nguyễn Anh Đô <nguyenanhdo200213@gmail.com>
2021-12-14 22:16:39 +0700

    modified and view file

--- circular_linked_list/main.cpp
+++ circular_linked_list/main.cpp
@@ -2,9 +2,54 @@
 
 using namespace std;
 
+struct Node {
+    int value;
+    Node *next;
+};
 
+struct CircularLinkedList {
+    Node *head;
+    Node *tail;
+};
+
+void init(CircularLinkedList &ls);
 
 int main() {
 
+    CircularLinkedList ls;
+    init(ls);
+
     return 0;
+}
+
+void init(CircularLinkedList &ls) {
+    ls.head = NULL;
+    ls.tail = NULL;
+}
+
+Node* createNode(int value) {
+    Node* p = new Node;
+    p->value = value;
+    p->next = NULL;
+    
+    return p;
+}
+
+void addFirst(CircularLinkedList &ls, int value) {
+    
+    Node *p = createNode(value);
+
+    if (ls.head == NULL) {
+        ls.head = ls.tail = p;
+        ls.tail->next = ls.head;
+    } else {
+        p->next = ls.head;
+        ls.head = p;
+        ls.tail->next = ls.head;
+    }
+
+}
+
+void printNode(const CircularLinkedList ls) {
+    
 }
